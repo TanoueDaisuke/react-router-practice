b686a96f77aaddfd7f2202fdde8e92c216571243
TanoueDaisuke <tano.dai.0711@icloud.com>
2020-06-03 09:12:27 +0900

    ログインフォームのスタイリング

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
 import { useHistory } from 'react-router-dom'
+import styled from 'styled-components'
 
 const Login = props => {
   const history = useHistory()
+
+  const Button = styled.button`
+    background: Bisque;
+    font-size: 1em;
+    margin: 1em;
+    padding: 0.25em 1em;
+    border: 2px solid Chocolate;
+    border-radius: 3px;
+  `
+
+  const Input = styled.input`
+    margin: 0.25rem;
+  `
 
   const onSubmit = (e) => {
     e.preventDefault()
@@ -15,12 +29,12 @@ const Login = props => {
       <h2>Login</h2>
       <form onSubmit={onSubmit}>
         <div>
-          username: <input type="text"/>
+          username: <Input type="text"/>
         </div>
         <div>
-          password: <input type="password" />
+          password: <Input type="password" />
         </div>
-        <button type="submit">login</button>
+        <Button type="submit">login</Button>
       </form>
     </div>
   )
